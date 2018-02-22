cf306817000a1dc7cfff5b80250913522abdb896
Elei-Mi-Shill <35346494+Elei-Mi-Shill@users.noreply.github.com>
2018-02-22 18:56:03 +0100

    Prima release

---
+++ InternalVM/src/InternalVM/Parser/EParseTokenType.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InternalVM.Parser;
+
+/**
+ *
+ * @author luca.scarcia
+ */
+public enum EParseTokenType {
+    VARIABLE,
+    METHOD,
+    PROPERTY,
+    ARRAY_PROPERTY;
+}
